73ea22fe5e30dd6aabe0faea7a1969e52678edae
Jake Laurie <jake.gordon.laurie@gmail.com>
2023-05-14 10:50:49 +0800

    Add mutation test

--- server/src/test/AnnounceController.test.ts
+++ server/src/test/AnnounceController.test.ts
@@ -21,16 +21,42 @@ const testClientState: ClientState = {
     }
 };
 
-test("announce", () => {
-    announceController.onAnnounce({
-        clientId: testClientId,
-        state: testClientState
+describe("AnnounceController", () => {
+    test("mutation server remove rule", () => {
+        announceController.onAnnounce({
+            clientId: testClientId,
+            state: testClientState
+        });
+
+        const result = announceController.onAnnounce({
+            clientId: testClientId,
+            state: {
+                ...testClientState,
+                rules: {}
+            }
+        });
+
+        expect(result).toEqual({
+            status: "ok",
+            mutation: {
+                add: [
+                    testClientState.rules["1"]
+                ]
+            }
+        });
     });
-    
-    const getAll = announceController.onGet();
-    
-    expect(getAll).toEqual([{
-        id: testClientId,
-        ...testClientState
-    }]);
+
+    test("Get returns all states", () => {
+        announceController.onAnnounce({
+            clientId: testClientId,
+            state: testClientState
+        });
+
+        const getAll = announceController.onGet();
+
+        expect(getAll).toEqual([{
+            id: testClientId,
+            ...testClientState
+        }]);
+    });
 });
